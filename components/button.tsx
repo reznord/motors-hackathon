b19d70d341ddaf3425d55291eedecf1d7654007d
Tiago Moutinho <tiago.moutinho@olx.com>
2021-05-21 11:55:34 +0100

    Cars

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
 export default function Button(props) {
   return (
     <a
-      className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg focus:ring-2 focus:ring-black"
+      className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg cursor-pointer"
       {...props}
     />
   );
