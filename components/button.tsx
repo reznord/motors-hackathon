22b2a32131dfcbc8a680ebf7bdc5c8ef5f03fac4
Rafael Sá <rafael.sa@olx.com>
2021-05-21 12:09:34 +0100

    Intro UX

--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
-export default function Button(props) {
+export default function Button({ className = "", ...props }) {
   return (
     <a
-      className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg cursor-pointer"
+      className={`text-white bg-blue-700 border-0 py-2 px-8 focus:outline-none hover:bg-blue-800 rounded text-lg focus:ring-2 focus:ring-black ${className}`}
       {...props}
     />
   );
