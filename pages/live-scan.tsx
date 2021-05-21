7474a339b1d7ca4795b7f7920dd04436d733e3aa
Rafael Sá <rafael.sa@olx.com>
2021-05-21 18:21:09 +0100

    Make live scan to work partially

---
+++ pages/live-scan.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs";
+
+function LiveScan() {
+  const videoRef = React.useRef();
+  const canvasRef = React.useRef();
+
+  React.useEffect(() => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "environment",
+          },
+        })
+        .then((stream) => {
+          window.stream = stream;
+          videoRef.current.srcObject = stream;
+
+          return new Promise((resolve, reject) => {
+            videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+      const modelPromise = cocoSsd.load();
+      Promise.all([modelPromise, webCamPromise])
+        .then((values) => {
+          detectFrame(videoRef.current, values[0]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, []);
+
+  const detectFrame = (video, model) => {
+    model.detect(video).then((predictions) => {
+      renderPredictions(predictions);
+      requestAnimationFrame(() => {
+        detectFrame(video, model);
+      });
+    });
+  };
+
+  const renderPredictions = (predictions) => {
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  };
+
+  return (
+    <div className="relative">
+      <video
+        autoPlay
+        playsInline
+        muted
+        ref={videoRef}
+        style={{
+          margin: "auto",
+          minWidth: "100%",
+          minHeight: "100%",
+          width: "auto",
+          height: "auto",
+          position: "fixed",
+          backgroundColor: "black",
+        }}
+      />
+      <canvas className="size" ref={canvasRef} className="absolute inset-0" />
+    </div>
+  );
+}
+
+export default LiveScan;
